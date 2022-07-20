fb755e1eda8797c1aa4b74c01db68a004af90fb6
Víctor Mar <godarkside@hotmail.com>
2022-07-19 19:01:02 -0500

    Creación de scripts componentes y cambios en HolaMunda
    -Se crearon tres nuevos scripts
    -Se agregaron funciones al script hola mundo

--- ProyectoInicialEBAC/Assets/Scripts/HolaMundo.cs
+++ ProyectoInicialEBAC/Assets/Scripts/HolaMundo.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class HolaMundo : MonoBehaviour
 {
+    int x;
+    private void Awake()
+    {
+        Debug.Log("Hola desde Awake");
+    }
     // Start is called before the first frame update
     void Start()
     {
+        x = 0;
         print("Hola Mundo");
         Debug.Log("Algo Paso");
         Debug.LogWarning("Algo Salio Medio Mal");
@@ -16,6 +22,25 @@ public class HolaMundo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //x = x + 1;
+        //Debug.Log(x);
+        Debug.Log("Hola desde Update");
+    }
+    private void FixedUpdate()
+    {
+        Debug.LogWarning("Hola desdes Fixed Update cada 50 frames");
+    }
+
+    private void LateUpdate()
+    {
+        Debug.Log("Hola desde Late Update");
+    }
+    private void OnEnable()
+    {
+        Debug.LogWarning("El objeto ha sido habilitado");
+    }
+    private void OnDisable()
+    {
+        Debug.LogWarning("El objeto ha sido inhabilitado");
     }
 }
